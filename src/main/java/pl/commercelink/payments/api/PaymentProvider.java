d23a6281304c2b4192b97bc2686c78e77a99938a
Marcin Dziedzic <marcin@madmangaming.pl>
2026-05-16 15:18:39 +0200

    add flexible PaymentLink structure

--- src/main/java/pl/commercelink/payments/api/PaymentProvider.java
+++ src/main/java/pl/commercelink/payments/api/PaymentProvider.java
@@ -1,6 +1,6 @@
 package pl.commercelink.payments.api;
 
 public interface PaymentProvider {
-    String createPaymentLink(PaymentRequest request);
+    PaymentLink createPaymentLink(PaymentRequest request);
     PaymentWebhookResult processWebhook(PaymentWebhookRequest request);
 }
